5f582a03d485f83ba9af3697b19f58f2376f98b8
TDK74 <98406593+TDK74@users.noreply.github.com>
2024-10-03 20:29:07 +0000

    new operations are added - K-nearest Neighbors and Euclidean Distance

--- Ch1_2_Intro_K-Nearest_Neighbors.py
+++ Ch1_2_Intro_K-Nearest_Neighbors.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import numpy as np
 
 dc_listings = pd.read_csv('dc_airbnb.csv')
 print(dc_listings.iloc[0])
+
+our_acc_values = 3
+first_living_space_valus = dc_listings.iloc[0]['accommodates']
+first_distance  = np.abs(first_living_space_valus - our_acc_values)
+print(first_distance)
