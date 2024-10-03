b720f211f9a507fb434a0b8a2847804b846a8d82
TDK74 <98406593+TDK74@users.noreply.github.com>
2024-10-03 20:38:34 +0000

    a new feature is added - Calculate Distance for All Observations

--- Ch1_2_Intro_K-Nearest_Neighbors.py
+++ Ch1_2_Intro_K-Nearest_Neighbors.py
@@ -8,3 +8,8 @@ our_acc_values = 3
 first_living_space_valus = dc_listings.iloc[0]['accommodates']
 first_distance  = np.abs(first_living_space_valus - our_acc_values)
 print(first_distance)
+
+new_listings = 3
+dc_listings['distance'] = dc_listings['accommodates'].apply(lambda x: np.abs(x - new_listings))
+print(dc_listings['distance'].value_counts())
+print(dc_listings[dc_listings['distance'] == 0]['accomodates'])
