13b337ca1dac10c54629ebb9abfd1f0876ac40c1
TDK74 <98406593+TDK74@users.noreply.github.com>
2024-10-09 11:45:25 +0000

    Added: Handling Missing Values.

--- Ch1_4_Multivariate_K-Nearest_Neighbors.py
+++ Ch1_4_Multivariate_K-Nearest_Neighbors.py
@@ -11,8 +11,13 @@ stripped_dollars = stripped_commas.str.replace('$', '')
 dc_listings['price'] = stripped_dollars.astype('float')
 print(dc_listings.info())
 
-# Removing Features
+# Removing Features from the DataFrame
 drop_columns = ['room_type', 'city', 'state', 'latitude', 'longitude', 'zipcode',
                 'host_response_rate', 'host_acceptance_rate', 'host_listings_count']
 dc_listings = dc_listings.drop(drop_columns, axis=1)
+print(dc_listings.isnull().sum())
+
+# Handling Missing Values
+dc_listings = dc_listings.drop(['cleaning_fee', 'security_deposit'], axis=1)
+dc_listings = dc_listings.dropna(axis=0)
 print(dc_listings.isnull().sum())
