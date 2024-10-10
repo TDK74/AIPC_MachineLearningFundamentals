e42cbcf6f920b41f6944502730fcc1cceaa3bbc6
TDK74 <98406593+TDK74@users.noreply.github.com>
2024-10-10 20:45:08 +0000

    Added: Holdout Validation + error metrics - iteration RMSEs and average RMSE.

--- Ch1_6_Cross_Validation.py
+++ Ch1_6_Cross_Validation.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.neighbors import KNeighborsRegressor as KNR
+from sklearn.metrics import mean_squared_error as MSE
 
 
 dc_listings = pd.read_csv("dc_airbnb.csv")
@@ -12,3 +14,27 @@ dc_listings = dc_listings.reindex(shuffled_index)
 # splitting dataset 50/50 for holdout validation
 split_one = dc_listings.iloc[0 : 1862].copy()
 split_two = dc_listings.iloc[1862 : ].copy()
+
+# Holdout Validation
+train_one = split_one
+test_one = split_two
+train_two = split_two
+test_two = split_one
+
+# First half
+model = KNR()
+model.fit(train_one[['accommodates']], train_one['price'])
+test_one['predicted_price'] = model.predict(test_one[['accommodates']])
+iteration_one_rmse = MSE(test_one['price'], test_one['predicted_price']) ** 0.5
+
+# Second half
+model.fit(train_two[['accommodates']], train_two['price'])
+test_two['predicted_price'] = model.predict(test_two[['accommodates']])
+iteration_two_rmse = MSE(test_two['price'], test_two['predicted_price']) ** 0.5
+
+# Average RMSError
+avg_rmse = np.mean([iteration_two_rmse, iteration_one_rmse])
+
+print(f"Iteration one RMSE: {iteration_one_rmse}")
+print(f"Iteration two RMSE: {iteration_two_rmse}")
+print(f"Average RMSE: {avg_rmse}")
