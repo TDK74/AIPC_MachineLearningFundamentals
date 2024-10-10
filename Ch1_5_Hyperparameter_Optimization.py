17eb7e7751a272f8f51bbcbb8e070b9a85ee6eed
TDK74 <98406593+TDK74@users.noreply.github.com>
2024-10-10 12:58:56 +0000

    Added: Hyperparameter Optimization.

--- Ch1_5_Hyperparameter_Optimization.py
+++ Ch1_5_Hyperparameter_Optimization.py
@@ -6,3 +6,18 @@ from sklearn.metrics import mean_squared_error as MSE
 from sklearn.metrics import root_mean_squared_error as RMSE
 
 
+# Hyperparameter Optimization
+train_df = pd.read_csv('dc_airbnb_train.csv')
+test_df = pd.read_csv('dc_airbnb_test.csv')
+features = ['accommodates', 'bedrooms', 'bathrooms', 'number_of_reviews']
+hyper_params = [1, 2, 3, 4, 5]
+mse_values = list()
+
+for hp in hyper_params:
+    knn = KNR(n_neighbors=hp, algorithm='brute')
+    knn.fit(train_df[features], train_df['price'])
+    predictions = knn.predict(test_df[features])
+    mse = MSE(test_df['price'], predictions)
+    mse_values.append(mse)
+
+print(mse_values)
