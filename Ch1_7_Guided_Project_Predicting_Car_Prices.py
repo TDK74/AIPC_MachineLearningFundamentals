92d5046d75539e27075469ed9d7c9ab07883541e
TDK74 <98406593+TDK74@users.noreply.github.com>
2024-10-13 11:00:36 +0000

    Added: Introduction To The Dataset.

--- Ch1_7_Guided_Project_Predicting_Car_Prices.py
+++ Ch1_7_Guided_Project_Predicting_Car_Prices.py
@@ -5,12 +5,22 @@ import numpy as np
 # dataset is used from here: https://archive.ics.uci.edu/dataset/10/automobile and all credits go to
 # Creators - Jeffrey Schlimmer
 
+# Introduction To The Dataset
 pd.options.display.max_columns = 99
 
 cols = ['symboling', 'normalized-losses', 'make', 'fuel-type', 'aspiration', 'num-of-doors', 'body-style',
         'drive-wheels', 'engine-location', 'wheel-base', 'length', 'width', 'height', 'curb-weight',
         'engine-type', 'num-of-cylinders', 'engine-size', 'fuel-system', 'bore', 'stroke', 'compression-rate',
         'horsepower', 'peak-rpm', 'city-mpg', 'highway-mpg', 'price']
-cars = pd.read_csv('import-85.data', names=cols)
+cars = pd.read_csv('imports-85.data', names=cols)
 
 cars.head()
+
+# Select only the columns with continuous values from imports-85.names
+continuous_values_columns = ['normalized-losses', 'wheel-base', 'length', 'width', 'height', 'curb-weight',
+                             'engine-size', 'bore', 'stroke', 'compression-rate', 'horsepower', 'peak-rpm',
+                             'city-mpg', 'highway-mpg', 'price']
+numeric_cars = cars[continuous_values_columns]
+
+numeric_cars.head()
+
