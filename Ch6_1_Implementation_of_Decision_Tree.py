d276de0ada1453a2485569d5773936b8fb747abb
TDK74 <98406593+TDK74@users.noreply.github.com>
2024-12-27 11:27:47 +0200

    Ch6_1_Implementation_of_Decision_Tree.py: Implemented Fitting a Decision-Tree algorithm to the Training set.

--- Ch6_1_Implementation_of_Decision_Tree.py
+++ Ch6_1_Implementation_of_Decision_Tree.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as mtp
 from sklearn.model_selection import train_test_split as tts
 from sklearn.preprocessing import StandardScaler as StScl
+from sklearn.tree import DecisionTreeClassifier as DcsTCls
 
 
 # importing dataset
@@ -22,3 +23,7 @@ x_test = st_x.transform(x_test)
 # print(x_train[:10])
 # print('\n')
 # print(x_test[:10])
+
+# fitting Decision Tree Classifier to the training set
+classifier = DcsTCls(criterion='entropy', random_state=0)
+classifier.fit(x_train, y_train)
