c8469389d2f05b6f8c729748e860090882297a81
TDK74 <98406593+TDK74@users.noreply.github.com>
2024-12-27 23:13:04 +0200

    Ch6_1_Implementation_of_Decision_Tree.py: Implemented Visualizing the training set result

--- Ch6_1_Implementation_of_Decision_Tree.py
+++ Ch6_1_Implementation_of_Decision_Tree.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as mtp
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split as tts
 from sklearn.preprocessing import StandardScaler as StScl
 from sklearn.tree import DecisionTreeClassifier as DcsTCls
 from sklearn.metrics import confusion_matrix as c_mtx
+from matplotlib.colors import ListedColormap as LstClm
 
 
 # importing dataset
@@ -34,3 +35,24 @@ y_pred = classifier.predict(x_test)
 
 # creation of Confusion matrix
 cm = c_mtx(y_test, y_pred)
+
+# visualizing the training set result
+x_set, y_set = x_train, y_train
+x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
+                            np.arange(start=x_set[:, 1].min() - 1, stop=x_set[:, 1].max() + 1, step=0.01))
+
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha=0.75, cmap=LstClm(('purple', 'green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1], c=LstClm(('purple', 'green'))(i), label=j)
+
+plt.title('Decision Tree Classification (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
